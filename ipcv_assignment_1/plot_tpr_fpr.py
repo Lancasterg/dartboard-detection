5d98b53adab238b9d9504b25c6a37449803bf7c7
George Lancaster <george@eduroam-135-22.nomadic.bris.ac.uk>
2018-11-10 16:40:05 +0000

    started task two

--- ipcv_assignment_1/plot_tpr_fpr.py
+++ ipcv_assignment_1/plot_tpr_fpr.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
-import csv
 
 
 def main():
-    with open('train_results.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in reader:
-            print(row)
+    tpr = [1, 1, 1]
+    fpr = [1, 0.0603282, 0.00877532]
+
+    for x in range(0, len(tpr)):
+        plt.scatter(fpr[x], tpr[x], label="Stage: {}".format(x))
+
+    plt.xlabel("False positive rate")
+    plt.ylabel("True positive rate")
+    plt.ylim([0, 1.1])
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
